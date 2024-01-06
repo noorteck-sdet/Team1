e4de38d4bd7e3702b47adf6c907d4a11bbc3eaf6
IRIS Donation <IRIS Donation@10.0.0.130>
2024-01-05 22:17:34 -0500

    3AM Set the timer

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,4 +4,12 @@ public class HWDemo {
 
 	//Instruction: Each team member pushes at least once by adding 1 method
 	//Practice Cloning, Pulling, Pushing, Stashing
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Mabood Please Push a line of code at Saturday 3AM");
+		// Lets do it i will push one too at 3AM 
+		
+	}
 }
