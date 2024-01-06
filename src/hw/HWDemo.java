926eefadd618b62bc9723e18b4804084d0850a39
Karina <Karina@10.0.0.31>
2024-01-05 23:22:04 -0500

    Hobbies TreeMap

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -1,5 +1,7 @@
 package hw;
 
+import java.util.TreeMap;
+
 public class HWDemo {
 
 	//Instruction: Each team member pushes at least once by adding 1 method
@@ -11,10 +13,16 @@ public class HWDemo {
 		System.out.println("Everyone!!!!! Please Push a line of code at Saturday 3AM");
 		// Lets do it i will push one too at 3AM 
 		
+		
+		
+	TreeMap<String,String>team1Hobbies= new TreeMap<String,String>();
+	
+	team1Hobbies.put("Karina","Dancing");
+	team1Hobbies.put("Noor", null);
+	team1Hobbies.put("Maboob", null);
+	team1Hobbies.put("Gustavo", null);
+	
+	
 	}
-
-	//Team1 hobbies before Java
-
-
 	
 }
