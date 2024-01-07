bcd9351e240a40e35411a85c9422fd83f93b9121
IRIS Donation <IRIS Donation@10.0.0.130>
2024-01-07 09:35:12 -0500

    Noor

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -11,5 +11,15 @@ public class HWDemo {
 		System.out.println("Everyone!!!!! Please Push a line of code at Saturday 3AM");
 		// Lets do it i will push one too at 3AM 
 		
-	}
+		
+		// Its Noor Zazai and I pushed the code at 3AM Lol
+		
+  	}
+	 public void mee(int abc, int bca) {
+		   System.out.println(abc + bca);
+		   
+		   
+		  
+		   }
+
 }
